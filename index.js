7ac41b3d7c397e19b976a388dbc24ff7036bfc9a
Sam Winslow <sammywinslow@gmail.com>
2021-11-16 17:41:16 -0500

    ISO string validation

--- index.js
+++ index.js
@@ -28,14 +28,24 @@ export async function setupPlugin({ config, global, storage }) {
         }
     }
     if (initial_last_invoked_at) {
-        const initialLastInvokedAt = parseInt(initial_last_invoked_at)
-        if (isNaN(initialLastInvokedAt)) {
-            throw new Error('initial_last_invoked_at is not a number.')
+        if (!isIsoDate(initial_last_invoked_at)) {
+            throw new Error('initial_last_invoked_at is not a valid ISO date.')
         }
-        await storage.set('last_invoked_at', initialLastInvokedAt)
+        await storage.set('last_invoked_at', initial_last_invoked_at)
     } else {
-        await storage.set('last_invoked_at', new Date().valueOf())
+        const now = new Date().toISOString()
+        console.log(`No initial_last_invoked_at set; using ${now}`)
+        await storage.set('last_invoked_at', now)
     }
+}
+
+function isIsoDate(str) {
+    // Checks if string is in the format 'YYYY-MM-DDTHH:MM:SS.mmmZ' (ISO 8601)
+    if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) {
+        return false
+    }
+    const d = new Date(str)
+    return d.toISOString() === str
 }
 
 function appendLastInvoked(url, last_invoked_at) {
@@ -43,10 +53,14 @@ function appendLastInvoked(url, last_invoked_at) {
 }
 
 async function callAPI({ url, include_last_invoked_at, http_method: method, headers = {}, body }, last_invoked_at) {
-    if (include_last_invoked_at && last_invoked_at !== undefined) {
+    if (include_last_invoked_at) {
+        if (!isIsoDate(last_invoked_at)) {
+            throw new Error(`"${last_invoked_at}" is not a valid ISO date; refusing to call API.`)
+        }
         url = appendLastInvoked(url, last_invoked_at)
     }
-    await fetch(url, {
+    // Don't await this, as we don't want to block the plugin
+    fetch(url, {
         method,
         headers,
         body: body || undefined,
@@ -54,7 +68,7 @@ async function callAPI({ url, include_last_invoked_at, http_method: method, head
 }
 
 function logRequest(last_invoked_at) {
-    console.log(`Sending request (Last invoked at: ${new Date(last_invoked_at).toISOString()})`)
+    console.log(`Sending request (Last invoked at: ${last_invoked_at})`)
 }
 
 export async function runEveryMinute({ config, global, storage }) {
@@ -63,7 +77,7 @@ export async function runEveryMinute({ config, global, storage }) {
     }
     const last_invoked_at = await storage.get('last_invoked_at')
     logRequest(last_invoked_at)
-    const now = new Date().valueOf()
+    const now = new Date().toISOString()
     await callAPI(global, last_invoked_at)
     await storage.set('last_invoked_at', now)
 }
@@ -74,7 +88,7 @@ export async function runEveryHour({ config, global, storage }) {
     }
     const last_invoked_at = await storage.get('last_invoked_at')
     logRequest(last_invoked_at)
-    const now = new Date().valueOf()
+    const now = new Date().toISOString()
     await callAPI(global, last_invoked_at)
     await storage.set('last_invoked_at', now)
 }
@@ -85,7 +99,7 @@ export async function runEveryDay({ config, global, storage }) {
     }
     const last_invoked_at = await storage.get('last_invoked_at')
     logRequest(last_invoked_at)
-    const now = new Date().valueOf()
+    const now = new Date().toISOString()
     await callAPI(global, last_invoked_at)
     await storage.set('last_invoked_at', now)
 }
